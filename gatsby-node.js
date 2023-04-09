1decd46328554a57c5e4d846444b497151e1a991
Deniz <deasign1@gmail.com>
2023-04-09 18:38:50 -0400

    packages

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,6 +7,21 @@ exports.onCreateWebpackConfig = ({
     actions,
   }) => {
     actions.setWebpackConfig({
+      resolve: {
+        fallback: {
+          fs: false
+          ,stream: false
+          ,tty:false
+          ,path: false
+          ,vm:false
+          ,http:false
+          ,https:false
+          ,zlib:false
+          ,os:false
+          ,crypto:false
+        }
+      }
+      ,
     externals: {
       canvas: "commonjs canvas" // Important (2)
       }
