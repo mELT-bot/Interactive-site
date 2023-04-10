a5335047b98ad00dfff83d7119aa0f6872abf049
Deniz <deasign1@gmail.com>
2023-04-09 20:45:47 -0400

    encoding polyfill

--- gatsby-node.js
+++ gatsby-node.js
@@ -28,7 +28,7 @@ exports.onCreateWebpackConfig = ({
           ,tls: false
           ,child_process:false
           ,perf_hooks:false
-          ,util: require.resolve('text-encoding-polyfill')
+          ,util: false
         }
       }
       ,
